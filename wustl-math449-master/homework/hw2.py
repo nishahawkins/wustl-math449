e934a45e75798035ff3b96905e3db611a80ce052
Nisha Hawkins <nishalhawkins@wustl.edu>
2020-10-15 17:42:30 -0500

    workin

--- wustl-math449-master/homework/hw2.py
+++ wustl-math449-master/homework/hw2.py
@@ -71,4 +71,29 @@ def plotConvergence(x_true, x, k=2, scale='log'):
     else:
         plt.plot(error)
 
+    def jacobiIteration(A, b, x0=None, tol=1e-13, numIter=100):
+    '''
+    Jacobi iteraiton:
+    A: nxn matrix
+    b: (n,) vector
+    x0: initial guess
+    numIter: total number of iteration
+    tol: algorithm stops if ||x^{k+1} - x^{k}|| < tol
+    return: x
+    x: solution array such that x[i] = i-th iterate
+    '''
+    n = A.shape[0]
+    x = np.zeros((numIter+1, n))
+    if x0 is not None:
+        x[0] = x0
+    D, L, U = getAuxMatrix(A)
+    for k in range(numIter):
+        x[k+1] = ((L+U)@x[k])/D + b/D
+        if norm(x[k+1] - x[k]) < tol:
+            break
+    
+    return x[:k+1]
+
+x = jacobiIteration(A, b)
+
 
