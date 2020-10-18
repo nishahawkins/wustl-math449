5dcde3b6a69c42b5e70a83e1ae87c8c9765544b1
Nisha Hawkins <nishalhawkins@wustl.edu>
2020-10-18 17:01:56 -0500

    working

--- wustl-math449-master/homework/hw2.py
+++ wustl-math449-master/homework/hw2.py
@@ -27,10 +27,10 @@ def getMatrix(n=10, isDiagDom=True):
     lower[:] = -1  #1
     upper[:] = -1  #1
     # Insert boundary conditions
-    diagonal[0] = 1
-    upper[0] = 0
-    diagonal[n] = 1
-    lower[-1] = 0
+    # diagonal[0] = 1
+    # upper[0] = 0
+    # diagonal[n] = 1
+    # lower[-1] = 0
 
     
     A = diags(
@@ -52,26 +52,7 @@ def getAuxMatrix(A):
     U = -triu(A, k=1)
     return D, L, U
 
-def plotConvergence(x_true, x, k=2, scale='log'):
-    '''
-    Plot the error ||x[i] - x_true||_k against i
-    x_true: (n,) vector
-    x: (m, n) matrix, x[i] is the i-th iterate, 
-    x[0] = x0 is the initial guess
-    m: total number of iterations
-    scale: 'log' or 'linear'
-    k: 1,2,..., of Inf
-    '''
-    numIter = x.shape[0]
-    error = np.zeros(numIter)
-    for i in range(numIter):
-        error[i] = norm(x_true - x[i], k)
-    if scale is 'log':
-        plt.semilogy(error)
-    else:
-        plt.plot(error)
-
-    def jacobiIteration(A, b, x0=None, tol=1e-13, numIter=100):
+def jacobiIteration(A, b, x0=None, tol=1e-13, numIter=100):
     '''
     Jacobi iteraiton:
     A: nxn matrix
@@ -94,6 +75,45 @@ def plotConvergence(x_true, x, k=2, scale='log'):
     
     return x[:k+1]
 
+A = getMatrix(n=5, isDiagDom=False)
+A = A/2
+print(A.toarray())
+x_true = np.random.randn(5)
+b = A@x_true
 x = jacobiIteration(A, b)
 
+def plotConvergence(x_true, x, k=2, scale='log', rate=True):
+    '''
+    Plot the error ||x[i] - x_true||_k against i or the rate of the convergence
+    x_true: (n,) vector
+    x: (m, n) matrix, x[i] is the i-th iterate, 
+    x[0] = x0 is the initial guess
+    m: total number of iterations
+    scale: 'log' or 'linear'
+    k: 1,2,..., of Inf
+    rate: bool (optional) If true, plot the rate of the convergence
+    ||x[i+1] - x_true||_k/||x[i] - x_true||_k against
+    '''
+    numIter = x.shape[0]
+    error = np.zeros(numIter)
+    for i in range(numIter):
+        error[i] = norm(x_true - x[i], k)
+    if (scale is 'log') and (not rate):
+        plt.semilogy(error)
+    elif (scale is 'linear') and (not rate):
+        plt.plot(error)
+    elif (scale is 'log') and rate:
+        plt.plot(np.log(error[1:]/error[:-1]))
+    elif (scale is 'linear') and rate:
+        plt.plot(error[1:]/error[:-1])
 
+maxProbSize = 40
+numIter = 100
+for i in range(5, maxProbSize+1, 5):
+    A = getMatrix(n=i)
+    x_true = np.random.randn(i)
+    b = A@x_true
+    x = jacobiIteration(A, b, numIter=numIter)
+    plotConvergence(x_true, x)
+
+# %%
